70e073a3422e357fa2cf1cc26528b7f7927f2e02
Thomas Järvstrand <tjarvstrand@gmail.com>
2025-06-30 07:48:48 +0200

    Add example comment

--- packages/linesman_lint/example/lib/test.dart
+++ packages/linesman_lint/example/lib/test.dart
@@ -10,4 +10,6 @@ import 'more_imports/import4.dart';
 // expect_lint: linesman_lint
 import 'more_imports/more_imports/import5.dart';
 
+/// See analysis_options.yaml for the rule configuration.
+
 const int result = one + two + three + four + five;
